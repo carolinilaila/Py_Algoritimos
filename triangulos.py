0243f4c50f4ced79f9aae422e1ca8be6c34f3482
unknown <carolinilaila@hotmail.com>
2020-06-15 19:05:02 -0300

    add descrição

---
+++ triangulos.py
@@ -0,0 +1,14 @@
+# triangulos
+aresta1 = int(input('Insira o tamanho da aresta 1: '))
+aresta2 = int(input('Insira o tamanho da aresta 2: '))
+aresta3 = int(input('Insira o tamanho da aresta 3: '))
+
+if aresta1 + aresta2 > aresta3 and aresta3 + aresta2 > aresta1 and aresta1 + aresta3 > aresta2:
+    if aresta1 == aresta2 and aresta2 == aresta3:
+        print('Pode formar um triangulo equilatero.')
+    elif (aresta1 == aresta2 and aresta1 != aresta3) or (aresta2 == aresta3 and aresta2 != aresta1):
+        print('Pode formar um triangulo isosceles.')
+    elif aresta1 != aresta2 and aresta2 != aresta3 or aresta1 != aresta3:
+        print('Pode formar um triangulo escaleno.')
+else:
+    print('Não pode formar um triangulo!')
